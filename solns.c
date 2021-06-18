53988677cb68b4edd858d0c8e942ed0295d40b6a
jhansy29 <82075770+jhansy29@users.noreply.github.com>
2021-06-18 17:45:06 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,4 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 int max(int[] , int ){
+  int a,highest=0;
+  for(a=0;a<int;a++){
+    if(int[a] > highest )
+      highest = int[a];
+  }
+  return highest;
+}
+    
   
