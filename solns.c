c617875787b80d727612f9a1213e4b329126c4a4
jhansy29 <82075770+jhansy29@users.noreply.github.com>
2021-06-19 09:51:39 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,33 +1,33 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-int max(int[] , int ){
+int max(int x[] , int n){
   int a,highest=0;
-  for(a=0;a<int;a++){
-    if(int[a] > highest )
-      highest = int[a];
+  for(a=0;a<n;a++){
+    if(x[a] > highest )
+      highest = x[a];
   }
   return highest;
 }
     
-int min(int[] , int ){
+int min(int x[] , int n){
   int a,lowest=0;
-  for(a=0;a<int;a++){
-    if(int[a] < highest )
-      lowest = int[a];
+  for(a=0;a<n;a++){
+    if(x[a] < highest )
+      lowest = x[a];
   }
   return lowest;
 }
-float average(int[] ,int ){
+float average(int x[] ,int n){
   int a;
   float sum=0.0;
-  for(a=0;a<int ;a++){
-    sum+=int[a];
+  for(a=0;a<n ;a++){
+    sum+=x[a];
   }
-  sum= sum/int;
+  sum= sum/n;
   return sum;
 }
 
-int mode(int[], int){
+int mode(int x[], int n){
   int num=0,a, counts[];
   for(a=0;a<int;a++){
     if(int[a]==int[0])
